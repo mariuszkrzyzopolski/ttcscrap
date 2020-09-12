089d9fd2d21c6682c45fef4df2c8ecc3ad4f20ee
warhir <krzyzopolski.mariusz@gmail.com>
2020-09-12 14:20:44 +0200

    scrape one page and print results

---
+++ ttcscrap/spiders/ttcspider.py
@@ -0,0 +1,58 @@
+import scrapy
+import re
+
+
+def clean_result(data):
+    non_decimal = re.compile(r'[^\d.]+')
+    data = non_decimal.sub('', data)
+    return data
+
+
+def check_if_suggested_price(list):
+    original_item_list = list.css('span.gold-amount::text').getall()
+    item_list = []
+    len_of_list = len(original_item_list)
+    for item in original_item_list:
+        item_list.append(clean_result(item))
+    if len_of_list == 7:
+        data = {
+            'Min': item_list[0],
+            'Average': item_list[1],
+            'Max': item_list[2],
+            'Suggested-Min': item_list[3],
+            'Suggested-Max': item_list[4],
+            'No-Listings': item_list[5],
+            'No-Items': item_list[6]
+        }
+    elif len_of_list == 5:
+        data = {
+            'Min': item_list[0],
+            'Average': item_list[1],
+            'Max': item_list[2],
+            'No-Listings': item_list[3],
+            'No-Items': item_list[4]
+        }
+    elif len_of_list == 4:
+        data = {
+            'Min': item_list[0],
+            'Average': item_list[1],
+            'Max': item_list[2],
+            'No-Listings': item_list[3],
+        }
+    return data
+
+
+class TtcSpider(scrapy.Spider):
+    name = "TTC"
+    start_urls = ['https://eu.tamrieltradecentre.com/pc/Trade/SearchResult?ItemID=&SearchType=PriceCheck&page=1']
+
+    def parse(self, response):
+        rows = response.xpath('//tbody/tr[not(@class)]')
+
+        for row in rows:
+            yield {
+                'Quality': row.css('div').xpath('@class').get(),
+                'Name': row.css('div::text').get(),
+                'Level': clean_result(row.css(':nth-child(3) ::text')[1].get()),
+                'Price': check_if_suggested_price(row)
+            }
